6fd959cf392ee7b78b8b48d5986f328886a51c53
srivier <soleane.rivier@ynov.com>
2023-12-14 22:16:31 +0100

    feat : Add used verif function from hangman

--- serveur.go
+++ serveur.go
@@ -13,6 +13,7 @@ type Structure struct {
 	Hangman *HangManData
 	StrWord string
 	Status  string
+	Win     int
 }
 
 func Home(w http.ResponseWriter, r *http.Request, infos Structure) {
@@ -20,32 +21,31 @@ func Home(w http.ResponseWriter, r *http.Request, infos Structure) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	infos.StrWord = string(infos.Hangman.Word)
-	if infos.Hangman.IsThisTheWord(infos.StrWord) {
-		infos.Status = "WIN"
-	}
 	template.Execute(w, infos)
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("./pages/login.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	template.Execute(w, nil)
-}
-
-func hangmanHandler(w http.ResponseWriter, r *http.Request, info Structure) {
+func hangmanHandler(w http.ResponseWriter, r *http.Request, info *Structure) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+		info.Init()
 	}
 
 	letter := r.FormValue("input")
-
-	if info.Hangman.MainMecanics(letter) {
-		info.Status = "WIN"
+	if info.Hangman.UsedVerif(letter) {
+		info.Status = "USED"
+	} else {
+		if info.Hangman.MainMecanics(letter) {
+			info.Status = "WIN"
+		} else if info.Hangman.EndGame() {
+			if string(info.Hangman.Word) == info.Hangman.ToFind {
+				info.Status = "WIN"
+			} else {
+				info.Status = "LOSE"
+			}
+		} else {
+			info.Status = ""
+		}
 	}
+
 	info.StrWord = string(info.Hangman.Word)
 
 	// Redirect back to the main page
@@ -69,11 +69,10 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		Home(w, r, myStruct)
 	})
-	http.HandleFunc("/hangman", func(w http.ResponseWriter, r *http.Request) {
-		hangmanHandler(w, r, myStruct)
-	})
 
-	http.HandleFunc("/login", Login)
+	http.HandleFunc("/hangman", func(w http.ResponseWriter, r *http.Request) {
+		hangmanHandler(w, r, &myStruct)
+	})
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
