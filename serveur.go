fe75eda45b7fe0fba17654f1f960410dc6f62e84
srivier <soleane.rivier@ynov.com>
2023-12-14 18:39:38 +0100

    feat : Add hangmanHandler for the main mecanic of HangmanWeb

--- serveur.go
+++ serveur.go
@@ -12,6 +12,7 @@ import (
 type Structure struct {
 	Hangman *HangManData
 	StrWord string
+	Status  string
 }
 
 func Home(w http.ResponseWriter, r *http.Request, infos Structure) {
@@ -19,29 +20,61 @@ func Home(w http.ResponseWriter, r *http.Request, infos Structure) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	infos.StrWord = string(infos.Hangman.Word)
+	if infos.Hangman.IsThisTheWord(infos.StrWord) {
+		infos.Status = "WIN"
+	}
 	template.Execute(w, infos)
 }
 
-func Info(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("./pages/info.html", "./templates/footer.html", "./templates/header.html")
+func Login(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./pages/login.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 	template.Execute(w, nil)
 }
 
-func main() {
+func hangmanHandler(w http.ResponseWriter, r *http.Request, info Structure) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	letter := r.FormValue("input")
+
+	if info.Hangman.MainMecanics(letter) {
+		info.Status = "WIN"
+	}
+	info.StrWord = string(info.Hangman.Word)
+
+	// Redirect back to the main page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (myStruct *Structure) Init() {
 	var infos HangManData
 	infos.SetData()
 	infos.SetWord(ReadAllDico())
-	var myStruct Structure
 	myStruct.Hangman = &infos
 	myStruct.StrWord = string(infos.Word)
-	fmt.Println(myStruct.StrWord)
+	fmt.Println(myStruct.Hangman.ToFind)
+	myStruct.Status = ""
+}
+
+func main() {
+	var myStruct Structure
+	myStruct.Init()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		Home(w, r, myStruct)
 	})
-	http.HandleFunc("/hangman", Info)
+	http.HandleFunc("/hangman", func(w http.ResponseWriter, r *http.Request) {
+		hangmanHandler(w, r, myStruct)
+	})
+
+	http.HandleFunc("/login", Login)
+
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":8080", nil)
